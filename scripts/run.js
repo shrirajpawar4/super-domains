025f501e14c98c451cd403a8f4cf800a41ab2b77
Shriraj Pawar <68687869+shrirajpawar4@users.noreply.github.com>
2022-02-23 01:20:46 +0530

    set record and price

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,19 @@
 const main = async() => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory("Domains");
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log("Contract deployed to: ", domainContract.address);
+    console.log("Contract deployed by: ", owner.address);
+
+    const txn = await domainContract.register("doom");
+    txn.wait();
+
+    const domainOwner = await domainContract.getAddress("doom");
+    console.log("Domain owned by: ", domainOwner);
+
+    txn = await domainContract.connect(randomPerson).setRecord("doom", "It's my domain");
+    await txn.wait();
 
 }
 
